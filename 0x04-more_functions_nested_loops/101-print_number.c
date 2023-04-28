5fb79309aa60ad7019ab3963fe7e83add0bfb685
ShroukElsaman <shroukelsaman62@gmail.com>
2023-04-27 17:59:49 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - Entry point
+ * @n: number
+ * Description: to check if letter is uppercase
+ * Return: 1 if upper 0 else
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n == 0)
+		return;
+	print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
